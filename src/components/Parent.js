12f3742878f85330204880f8e9da668523ee71c8
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-08-30 06:17:45 +0000

    submit

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import Child from "./Child";
 
 function Parent() {
-    let [inputValue, setInputValue] = useState('');
+  const [parentInputValue, setParentInputValue] = useState('');
+  const [childInputValue, setChildInputValue] = useState('');
 
-    return(
-        <div>
-            <h1>Parent Component</h1>
-            <p>{inputValue}</p>
-            <Child setInputValue = {setInputValue} />
-        </div>
-    )
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <p>Parent Input Value: {parentInputValue}</p>
+      <Child childInputValue={childInputValue} setChildInputValue={setChildInputValue} />
+      <input
+        type="text"
+        value={parentInputValue}
+        onChange={(event) => {
+          setParentInputValue(event.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export default Parent;
