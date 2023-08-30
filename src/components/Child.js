5a45fb921c9ca5fd918cf7a890ad4409ccad2692
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-08-30 06:17:29 +0000

    submit

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function Child(props) {
-    
-    return(
-        <div>
-            <h1>Child Component</h1>
-            <input type="text" onChange={(event) => {
-                props.setInputValue(event.target.value)
-            }} ></input>
-        </div>
-    )
+  return (
+    <div className="child">
+      <h1>Child Component</h1>
+      <input
+        type="text"
+        value={props.childInputValue}
+        onChange={(event) => {
+          props.setChildInputValue(event.target.value);
+        }}
+      />
+    </div>
+  );
 }
 
 export default Child;
